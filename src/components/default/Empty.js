acc2a85a224746f4d9c4406607a68de58d032e53
Michael Jordan-Hotchkiss <mj@Michaels-Mac-mini.local>
2024-05-23 11:26:58 -0500

    added assets and components

---
+++ src/components/default/Empty.js
@@ -0,0 +1,10 @@
+const Empty = (props) => {
+    return (
+        <div className={`${props.className ? props.className : '' }`}>
+           
+            <p>{props.message ?? 'Empty'}</p>
+        </div>
+    );
+}
+
+export default Empty;
